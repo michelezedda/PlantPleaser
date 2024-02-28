c35cba248c4aa193d747432cbe8484ab87d68ccb
Michele <mikzed@hotmail.it>
2024-02-28 12:46:51 +0100

    aedaw

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,15 +1,77 @@
-import { useState } from "react";
-import Popular from "./Popular";
+import { useEffect, useState } from "react";
+import { Splide, SplideSlide } from "@splidejs/react-splide";
+
+const BASE_URL = "https://api.spoonacular.com/";
+const KEY = "6c999bc765be4b6fbe04290c62df9d3c";
+const DIET = "vegan";
 
 function Home() {
-  const [] = useState();
+  const [recipes, setRecipes] = useState([]);
+
+  useEffect(() => {
+    fetch(`${BASE_URL}recipes/random?apiKey=${KEY}&number=9&diet=${DIET}`)
+      .then((response) => {
+        if (response.ok) {
+          return response.json();
+        }
+        throw new Error("Network response was not ok.");
+      })
+      .then((data) => {
+        if (data && data.recipes) {
+          setRecipes(data.recipes);
+        } else {
+          console.error("Response format is incorrect or missing recipes data");
+        }
+      })
+      .catch((error) => {
+        console.error("Error fetching data:", error);
+      });
+  }, []);
 
   return (
     <>
       <div className="home-container">
-        <h2>Home</h2>
+        <div className="home-title-container">
+          <h2>Selection</h2>
+        </div>
+        <div className="select-menu">
+          <div>appetizer</div>
+          <div>main course</div>
+          <div>side dish</div>
+          <div>fingerfood</div>
+          <div>snack</div>
+          <div>bread</div>
+          <div>salad</div>
+          <div>dessert</div>
+          <div>beverage</div>
+        </div>
       </div>
-      <Popular />
+      <div className="popular-container">
+        <h2>Popular</h2>
+        <Splide
+          options={{
+            perPage: 4,
+            arrows: false,
+            pagination: false,
+            drag: "free",
+            gap: "5rem",
+          }}
+        >
+          <div className="card-container">
+            {recipes.map((recipe) => {
+              return (
+                <SplideSlide>
+                  <div className="card">
+                    <h3>{recipe.title}</h3>
+                    <img src={recipe.image} alt={recipe.title} />
+                    <button className="view-recipe-btn">View Recipe</button>
+                  </div>
+                </SplideSlide>
+              );
+            })}
+          </div>
+        </Splide>
+      </div>
     </>
   );
 }
