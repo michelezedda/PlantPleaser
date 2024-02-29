cb8c7a560a967ab626009d9a664f8a4a4ca2424e
Michele <mikzed@hotmail.it>
2024-02-29 12:26:33 +0100

    dwa

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,36 +1,34 @@
 import { useEffect, useState } from "react";
-import { Splide, SplideSlide } from "@splidejs/react-splide";
 import axios from "axios";
 
 function Home({ searchResults }) {
   const [recipes, setRecipes] = useState([]);
+  const [selectedRecipe, setSelectedRecipe] = useState(null);
 
-  const BASE_URL = "https://api.spoonacular.com/";
-  const KEY = "9916ddb00d084027bdf1766ef68da8dd";
-  const DIET = "vegan";
-  const NUMBER = "9";
+  const handleViewRecipe = (recipe) => {
+    setSelectedRecipe(recipe);
+  };
+
+  const fetchRecipes = async () => {
+    try {
+      const response = await axios.get(
+        `https://api.spoonacular.com/recipes/random`,
+        {
+          params: {
+            apiKey: "9e0c96ea51a24d6d86010dbcabec905f",
+            number: "9",
+            tags: "vegan",
+          },
+        }
+      );
+      setRecipes(response.data?.recipes || []);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
-    axios
-      .get(
-        `${BASE_URL}recipes/random?apiKey=${KEY}&number=${NUMBER}&tags=${DIET}`
-      )
-      .then((response) => {
-        if (response.status === 200) {
-          return response.data;
-        }
-        throw new Error("Network response was not ok.");
-      })
-      .then((data) => {
-        if (data && data.recipes) {
-          setRecipes(data.recipes);
-        } else {
-          console.error("Response format is incorrect or missing recipes data");
-        }
-      })
-      .catch((error) => {
-        console.error("Error fetching data:", error);
-      });
+    fetchRecipes();
   }, []);
 
   return (
@@ -55,27 +53,51 @@ function Home({ searchResults }) {
           <div key={result.id}>
             <h2>{result.title}</h2>
             <img src={result.image} alt={result.title} />
-            <p>{result.description}</p>
             <button className="view-recipe-btn">View Recipe</button>
           </div>
         ))}
       </div>
 
-      <div className="popular-container">
-        <h2>Popular</h2>
-        <div className="card-container">
-          {recipes.length > 0 ? (
-            recipes.map((recipe) => (
+      <div>
+        <div className="popular-container">
+          <h2>Popular</h2>
+          <div className="card-container">
+            {recipes.map((recipe) => (
               <div className="card" key={recipe.id}>
                 <h3>{recipe.title}</h3>
                 <img src={recipe.image} alt={recipe.title} />
-                <button className="view-recipe-btn">View Recipe</button>
+                <button
+                  className="view-recipe-btn"
+                  onClick={() => handleViewRecipe(recipe)}
+                >
+                  View Recipe
+                </button>
               </div>
-            ))
-          ) : (
-            <p>No recipes found</p>
-          )}
+            ))}
+          </div>
         </div>
+        {selectedRecipe && (
+          <div className="full-recipe">
+            <h2>{selectedRecipe.title}</h2>
+            <button onClick={() => setSelectedRecipe(null)}>X</button>
+            <img src={selectedRecipe.image} alt={selectedRecipe.title} />
+            <p>
+              <b>Servings:</b> {selectedRecipe.servings}{" "}
+            </p>
+            <p>
+              <b>Ready In:</b> {selectedRecipe.readyInMinutes} minutes
+            </p>
+            <p>
+              <b>Ingredients:</b>{" "}
+              {selectedRecipe.extendedIngredients
+                .map((ingredient) => ingredient.name)
+                .join(", ")}
+            </p>
+            <p>
+              <b>Instructions:</b> {selectedRecipe.instructions}
+            </p>
+          </div>
+        )}
       </div>
     </>
   );
