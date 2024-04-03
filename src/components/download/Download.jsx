829def3ff822fed310a934e39eb39100a02d4fd8
Michele <mikzed@hotmail.it>
2024-04-03 16:17:20 +0200

    changes

--- src/components/download/Download.jsx
+++ src/components/download/Download.jsx
@@ -1,9 +1,16 @@
 import "./download.css";
+import MyPhone from "../../../public/phone.png";
+import AppStore from "../../../public/app-store.png";
+import PlayStore from "../../../public/play-store.png";
 
 function Download() {
   return (
     <>
-      <div className="download-container"></div>
+      <div className="download-container">
+        <img src={MyPhone} alt="phone" />
+        <img src={AppStore} />
+        <img src={PlayStore} />
+      </div>
     </>
   );
 }
