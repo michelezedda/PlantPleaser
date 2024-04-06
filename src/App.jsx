7dded909664ade804fc2891d2d262326ecb712d5
Michele <mikzed@hotmail.it>
2024-04-06 12:44:12 +0200

    media queries

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,6 @@ import Sidebar from "./components/sidebar/Sidebar";
 import Navbar from "./components/navbar/Navbar";
 import MostRated from "./components/mostrated/MostRated";
 import Download from "./components/download/Download";
-import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import { Helmet } from "react-helmet";
 
@@ -17,7 +16,6 @@ function App() {
       </Helmet>
       <Navbar />
       <Sidebar />
-      <Header />
       <MostRated />
       <Download />
       <Footer />
