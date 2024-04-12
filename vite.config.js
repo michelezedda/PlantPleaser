5bac7bcbc266bab49be666daf1996fa9eae90a90
Michele <mikzed@hotmail.it>
2024-04-12 13:22:53 +0200

    env

--- vite.config.js
+++ vite.config.js
@@ -4,12 +4,4 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  define: {
-    "process.env.VITE_SPOONACULAR_KEY": JSON.stringify(
-      process.env.VITE_SPOONACULAR_KEY
-    ),
-  },
-  build: {
-    outDir: "dist",
-  },
 });
